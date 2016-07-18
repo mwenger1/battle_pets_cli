a086a028ab2bed343d4bac1882db38aa6e378216
Michael Wenger <michaelwenger27@gmail.com>
2016-07-17 23:54:33 -0400

    Display winner after battle

--- lib/arena_client.rb
+++ lib/arena_client.rb
@@ -1,17 +1,40 @@
 class ArenaClient
+  AFFIRMATIVE_WORDS = ["y", "yes"]
   ENDPOINT_URL = "http://localhost:7001/v1/competitions"
 
   def battle(challenger:, challenged:)
     competition = start_competition(challenger: challenger, challenged: challenged)
     say "\nYour BattlePets are competing in #{competition.competition_type}. The battle starts now!!"
+    check_score = ask "\nIt was an epic battle and the judges are consulting. Do you want to see if they are ready to announce the score? (y/n)"
+    if AFFIRMATIVE_WORDS.include? check_score.downcase
+      competition = check_competition_score(competition.id)
+
+      if competition.challenger == competition.winner
+        say "Congrats you won!"
+      elsif competition.challenged == competition.winner
+        say "Bummer you lost!"
+      else
+        say "Looks like the judges are still deliberating."
+      end
+    end
   end
 
   private
 
   def start_competition(challenger:, challenged:)
-    competition = JSON.parse(
+    convert_to_competition_object(
       RestClient.post ENDPOINT_URL, competition: { challenger: challenger, challenged: challenged }
     )
+  end
+
+  def check_competition_score(id)
+    convert_to_competition_object(
+      RestClient.get [ENDPOINT_URL, id].join("/")
+    )
+  end
+
+  def convert_to_competition_object(json)
+    competition = JSON.parse(json)
     Competition.new(symbolize_hash competition)
   end
 
--- lib/competition.rb
+++ lib/competition.rb
@@ -1,10 +1,11 @@
 class Competition
-  attr_reader :id, :competition_type, :challenger, :challenged
+  attr_reader :id, :competition_type, :challenger, :challenged, :winner
 
-  def initialize(id:, competition_type:, challenger:, challenged:, **)
+  def initialize(id:, competition_type:, challenger:, challenged:, winner: nil, **)
     @id = id
     @competition_type = competition_type
     @challenger = challenger
     @challenged = challenged
+    @winner = winner
   end
 end
